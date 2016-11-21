c397bd3fb103b67010b4e4adcc4e4be2f6dd624e
jasaaved <jasaaved@uci.edu>
2016-11-20 16:53:13 -0800

    time is now printed

--- cPlusPlusSource/AIShell.cpp
+++ cPlusPlusSource/AIShell.cpp
@@ -61,6 +61,7 @@ Move AIShell::minimax(int d) {
 }
 
 int AIShell::FindMin(int alpha, int beta, int d) {
+	std::cout << "Time left: " << time_left() - start_time << std::endl;
 	if (d == 1 || time_left() - start_time >= deadline - 500) {
 		for (int i = 0; i < numCols; i++) {
 			for (int j = 0; j < numRows; j++) {
@@ -107,6 +108,7 @@ int AIShell::FindMin(int alpha, int beta, int d) {
 }
 
 int AIShell::FindMax(int alpha, int beta, int d) {
+	std::cout << "Time left: " << time_left() - start_time << std::endl;
 	if (d == 1 || time_left() - start_time >= deadline - 500) {
 		for (int i = 0; i < numCols; i++) {
 			for (int j = 0; j < numRows; j++) {
