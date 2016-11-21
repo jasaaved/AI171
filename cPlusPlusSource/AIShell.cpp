6b3c0a896614ba38c3814901482f42db4463eee9
jasaaved <jasaaved@uci.edu>
2016-11-20 17:42:20 -0800

    fixed equation

--- cPlusPlusSource/AIShell.cpp
+++ cPlusPlusSource/AIShell.cpp
@@ -471,10 +471,9 @@ Move AIShell::makeMove()
 		deadline = 5000;
 	}
 
-	move_deadline - deadline;
+	move_deadline = deadline / 1000;
 	std::cout << move_deadline << std::endl;
-	move_deadline = (move_deadline)/10000 - 0.5;
-	std::cout << move_deadline << std::endl;
+	move_deadline = move_deadline - 0.5;
 	
 	return minimax(depth);	 
 }
